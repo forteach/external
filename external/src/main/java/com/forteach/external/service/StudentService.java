2d11b409b26d4264e37c54875675e6c15236b610
ang00 <zhang10092009@hotmail.com>
2019-01-07 17:39:59 +0800

    add redis

---
+++ external/src/main/java/com/forteach/external/service/StudentService.java
@@ -0,0 +1,12 @@
+package com.forteach.external.service;
+
+/**
+ * @Auther: zhangyy
+ * @Email: zhang10092009@hotmail.com
+ * @Date: 19-1-7 09:44
+ * @Version: 1.0
+ * @Description:
+ */
+public interface StudentService {
+    void saveAll();
+}
